9079ec8261cd30ef8f2a1c2ae26a9c6a698b945c
GabrielyMachado <gabriely.machado@hotmail.com>
2023-06-09 15:28:59 -0300

    feat: adicionada animação do formulário de login utilizando animatedContainer

--- lib/components/auth_form.dart
+++ lib/components/auth_form.dart
@@ -125,14 +125,12 @@ class _AuthFormState extends State<AuthForm>
     return Card(
       elevation: 8,
       shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(10)),
-      child: AnimatedBuilder(
-        animation: _heightAnimation!,
-        builder: (context, childForm) => Container(
-          padding: const EdgeInsets.all(16),
-          width: deviceSize.width * 0.75,
-          height: _heightAnimation?.value.height ?? (_isLogin() ? 310 : 400),
-          child: childForm,
-        ),
+      child: AnimatedContainer(
+        padding: const EdgeInsets.all(16),
+        width: deviceSize.width * 0.75,
+        height: _isLogin() ? 310 : 400,
+        duration: const Duration(milliseconds: 300),
+        curve: Curves.easeIn,
         child: Form(
           key: _formKey,
           child: Column(
