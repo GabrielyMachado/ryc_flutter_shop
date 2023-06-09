fc9520d93c5e91e4200944791df6c8ad3a7e5fbd
GabrielyMachado <gabriely.machado@hotmail.com>
2023-06-09 17:31:42 -0300

    feat: utilizando transição Hero

--- lib/components/product_grid_item.dart
+++ lib/components/product_grid_item.dart
@@ -71,15 +71,14 @@ class ProductGridItem extends StatelessWidget {
           ),
         ),
         child: GestureDetector(
-          child: FadeInImage(
-            placeholder: const AssetImage('assets/images/placeholder.png'),
-            image: NetworkImage(product.imageUrl),
-            fit: BoxFit.cover,
+          child: Hero(
+            tag: product.id,
+            child: FadeInImage(
+              placeholder: const AssetImage('assets/images/placeholder.png'),
+              image: NetworkImage(product.imageUrl),
+              fit: BoxFit.cover,
+            ),
           ),
-          // child: Image.network(
-          //   product.imageUrl,
-          //   fit: BoxFit.cover,
-          // ),
           onTap: () {
             Navigator.of(context).pushNamed(
               AppRoutes.productDetail,
